c9e52ce5a679c75674c1bc512093814a1e688b15
Deshawn Sambrano <DSambrano@g.harvard.edu>
2022-06-11 10:03:45 -0700

    Adding SSH, background image, and start up apps

--- mac/dock_setup.sh
+++ mac/dock_setup.sh
@@ -28,6 +28,7 @@ printf '%s\n' ' done.'
 
 # Allow Remote login with SSH
 sudo systemsetup -setremotelogin on
+# sudo launchctl load -w /System/Library/LaunchDaemons/ssh.plist # not sure if I need both
 
 # Iterm2 updates don't forget to update the ~/.zshrc to include each of the folloing as plugins:
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k # ZSH_THEME="powerlevel10k/powerlevel10k"
@@ -47,7 +48,7 @@ defaults write com.apple.Finder "AppleShowAllFiles" -bool "true" # Show hidden f
 defaults write com.apple.Finder _FXSortFoldersFirst -bool true # folders on top when sorting by name
 defaults write com.apple.Finder _FXShowPosixPathInTitle -bool true # show POSIX path
 defaults write com.apple.Finder WarnOnEmptyTrash -bool false # disable the warning before emptying the Trash
-
+# osascript -e 'tell application "Finder" to set desktop picture to POSIX file "<path/to/image>"' # set background image
 
 # Menu config
 defaults write com.apple.menuextra.clock "DateFormat" -string "\"EEE MMM d  h:mm:ss a\""  # https://macos-defaults.com/#🙋-what-s-a-defaults-command
@@ -56,7 +57,16 @@ defaults write -g AppleInterfaceStyle Dark # https://apple.stackexchange.com/que
 
 killall Dock
 
+# Set Start Up Apps
+osascript -e 'tell application "iTerm" to open the startup disk'
 
+
+
+
+
+
+
+# End script and restart 
 read -t 30 -p "The configs are complete. Although some will not be updated until the next reboot. Would you like to reboot now? [Y]/N" restart
 restart=${restart:-N} # If parameter is unset or null, the expansion of word is substituted. Else whatever was read in
 
