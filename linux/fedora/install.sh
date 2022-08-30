2f8b65dfaa9c41104a18f0341bc94f9390ac6438
Deshawn Sambrano <3dbrano3@gmail.com>
2022-08-30 12:05:16 -0700

    Swithing from Flatpak to Fusion Repos

--- linux/fedora/install.sh
+++ linux/fedora/install.sh
@@ -82,5 +82,7 @@ sudo dnf install xorg-x11-drv-nvidia-cuda -y # This is for cuda support (likely 
 
 ## Installing packages
 sudo dnf install gamemode discord steam mangohud gamescope
-# flatpak install flathub com.valvesoftware.Steam com.discordapp.Discord # not sure if I want fp version or nah 
+### flatpak install flathub com.valvesoftware.Steam com.discordapp.Discord # not sure if I want fp version or nah 
 flatpak install flathub net.davidotek.pupgui2 # net.lutris.Lutris com.heroicgameslauncher.hgl
+### Below is no longer needed because I am switching from flatpak to the fusion repos
+## Game Specific info: So it turns out that installing steam via flatpak moves things just a tad, but here is the location for apex's config file under flatpak install. You will want to change the profile/profile.cfg as well as videoconfig.txt and settings.cfg from the local folder. directory located: /home/buddy/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/compatdata/1172470/pfx/drive_c/users/steamuser/Saved Games/Respawn/Apex 
