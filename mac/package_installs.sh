dc432bd38ebad2ddcf274876db9296b7068138b9
dsambrano <3dbrano3@gmail.com>
2022-09-09 11:26:50 -0700

    Reoganizing Package Installs

--- mac/package_installs.sh
+++ mac/package_installs.sh
@@ -1,8 +1,57 @@
 #!/usr/bin/env bash
 
+# Installing Xcode and the package Manager
 sudo xcode-select --install || sudo xcode-select --reset || echo "XCode Already installed"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install git gh bat neofetch python fzf btop cmatrix xclip node ffmpeg duti lf nmap netcat docker-compose docker-machine trash-cli autojump tldr thefuck espeak tmux ranger wget nvim onefetch
-brew install --cask r rstudio amethyst mpv visual-studio-code sublime-text iterm2 firefox brave-browser xquartz docker virtualbox zoom slack spotify anaconda onlyoffice edex-ui itsycal insomnia
+# Installing CLI apps
+brew install git \
+             gh \
+             bat \
+             neofetch \
+             python \
+             fzf \
+             btop \
+             cmatrix \
+             xclip \
+             node \
+             ffmpeg \
+             duti \
+             lf \
+             nmap \
+             netcat \
+             docker-compose \
+             docker-machine \
+             trash-cli \
+             autojump \
+             tldr \
+             thefuck \
+             espeak \
+             tmux \
+             ranger \
+             wget \
+             nvim \
+             onefetch
+
+# Installing GUI APPs
+brew install --cask r \
+                    rstudio \
+                    amethyst \
+                    mpv \
+                    visual-studio-code \
+                    sublime-text \
+                    iterm2 \
+                    firefox \
+                    brave-browser \
+                    xquartz \
+                    docker \
+                    virtualbox \
+                    zoom \
+                    slack \
+                    spotify \
+                    anaconda \
+                    onlyoffice \
+                    edex-ui \
+                    itsycal \
+                    insomnia
 
