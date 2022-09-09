a3122bdd3a93496d44c80f299751e66ebc8adb97
dsambrano <3dbrano3@gmail.com>
2022-09-09 11:18:41 -0700

    Fixing Symlinks: Including proper Arguments and removing CPs in place for ln -s

--- install.sh
+++ install.sh
@@ -46,8 +46,11 @@ for i in $repos; do
 done
 cd -
 
+# Install vim-plug to use plugins for nvim: https://github.com/junegunn/vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 # Sym links require the clone of the main repo first to work. 
-source ./symlinks.sh	
+source ./symlinks.sh $OS_DIR
 
 
 ## Conditionals. This is where I would set up things like the GoXLR and smashbox/gaming setup etc
--- symlinks.sh
+++ symlinks.sh
@@ -1,24 +1,29 @@
 #!/usr/bin/env bash
 
+echo "$1"
+echo $@
 OS_DIR=$1
+echo "$OS_DIR"
 
 ## Set up Vim
 ## Need to adjust location for this 
 ln -s ~/git_repos/dotfiles/config/.vimrc ~/
-cp config/.vimrc ~/.vimrc # If I want to move this to ~/.config I can ln -s or follow this guide: https://vi.stackexchange.com/a/11881 
+#cp config/.vimrc ~/.vimrc # If I want to move this to ~/.config I can ln -s or follow this guide: https://vi.stackexchange.com/a/11881 
+echo "Before NVIM"
 
 ## NeoVim symlink and install plugins
 ln -s ~/git_repos/dotfiles/config/nvim ~/.config # Requires that the git repos are already cloned. So this may need to be adjusted. 
 ## need to move Plugin files only here
 nvim +PlugInstall +qall # Installing all Plugins
 ## need to move the rest of the files now: This will prevent the error for theming before installed
+echo "Before Aliases"
 
 ## Aliases symlinks and add them to zsh (Could be replaced in the furture with just symlinking the full .zsh and .p10k files over. Only fixes the echos not the symlinks, those would still need to be there) 
 mkdir -p ~/.aliases
 ln -s ~/git_repos/dotfiles/config/.aliases/aliases ~/.aliases/
 ln -s ~/git_repos/dotfiles/$OS_DIR/config/.aliases/package_aliases ~/.aliases/
-cp -r config/.aliases ~/ # Needs to be replaced with a symlink option and need to call in the OS_dir here so I can also get the package_aliases file as well
+#cp -r config/.aliases ~/ # Needs to be replaced with a symlink option and need to call in the OS_dir here so I can also get the package_aliases file as well
 echo "source ~/.aliases/aliases" >> ~/.zshrc # https://superuser.com/a/331766
 echo "source ~/.aliases/package_aliases" >> ~/.zshrc
 # Need to also add package_aliases I believe if I have that start with a `.` I can make a single `cp` line
-
+echo "EnD SYmLINKS"
