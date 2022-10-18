0765334030e5a16d56a0bafc5ae279990c095229
Deshawn Sambrano <dsambrano@users.noreply.github.com>
2022-10-17 21:14:54 -0700

    Installing Insomnia; Fixing typo

--- mac/install.sh
+++ mac/install.sh
@@ -6,7 +6,7 @@ set -e
 sudo xcode-select --install
 
 brew install git gh bat neofetch python fzf htop cmatrix xclip node ffmpeg duti lf nmap netcat docker docker-compose docker-machine trash-cli autojump tldr thefuck espeak tmux ranger
-brew install --cask r rstudio amethyst mpv visual-studio-code sublime-text iterm2 firefox brave-browser xquartz docker virtualbox zoom slack spotify anaconda onlyoffice edex-ui itsycal
+brew install --cask r rstudio amethyst mpv visual-studio-code sublime-text iterm2 firefox brave-browser xquartz docker virtualbox zoom slack spotify anaconda onlyoffice edex-ui itsycal insomnia
 
 git config --global user.name 'Deshawn Sambrano'
 git config --global user.email ''
