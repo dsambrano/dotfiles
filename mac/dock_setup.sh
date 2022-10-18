aa53595c6ae1f8c96ab4cf2bc0502d65d81b111e
Deshawn Sambrano <dsambrano@users.noreply.github.com>
2022-10-17 21:14:54 -0700

    Setting Hot Corners, Trackpad Gestures, and Generic settings (e.g, screen saver)

--- mac/dock_setup.sh
+++ mac/dock_setup.sh
@@ -46,6 +46,14 @@ defaults write com.apple.dock orientation left # Place on left
 defaults write com.apple.dock "autohide" -bool "true" # Auto hide
 defaults write com.apple.dock "autohide-time-modifier" -float "0" # No animation time
 defaults write com.apple.dock no-bouncing -bool true # Remove bouncing animation from: https://www.switchingtomac.com/tutorials/how-to-permanently-stop-dock-icons-from-bouncing/
+## Set hot corners https://wilsonmar.github.io/dotfiles/#desktop-scren-saver-hot-corners
+### Top right screen corner → Mission Control
+defaults write com.apple.dock wvous-tr-corner -int 2
+defaults write com.apple.dock wvous-tr-modifier -int 0
+### Top right screen corner → Put Display to Sleep
+defaults write com.apple.dock wvous-tr-corner -int 10
+defaults write com.apple.dock wvous-tr-modifier -int 0
+
 
 # Finder config
 defaults write NSGlobalDomain "AppleShowAllExtensions" -bool "true" # Show extensions
@@ -64,12 +72,68 @@ defaults write com.apple.menuextra.clock "DateFormat" -string "\"EEE MMM d  h:mm
 defaults write -g AppleInterfaceStyle Dark # https://apple.stackexchange.com/questions/391686/how-to-set-dark-mode-appearance-to-auto-in-terminal
 defaults write -g AppleHighlightColor "0.847059 0.847059 0.862745 Graphite" # Setting my highlight color
 
-# Set Mouse/Trackpad Speed
+# Mouse
+## Set Mouse/Trackpad Speed
 defaults write -g com.apple.trackpad.scaling -float 3.0
 defaults write -g com.apple.mouse.scaling -float 3.0
+## Set Trackpad Gestures https://superuser.com/a/868797 
+## More on defaults command: https://ss64.com/osx/defaults.html
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad DragLock -bool false
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Dragging -bool false
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadCornerSecondaryClick -bool false
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadFiveFingerPinchGesture -int 2
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadFourFingerHorizSwipeGesture -int 2
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadFourFingerPinchGesture -int 2
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadFourFingerVertSwipeGesture -int 2
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadHandResting -bool true
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadHorizScroll -bool true
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadMomentumScroll -bool true
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadPinch -bool true
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -bool true
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRotate -bool true
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadScroll -bool true
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -int 2
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerHorizSwipeGesture -int 2
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerTapGesture -int 2
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerVertSwipeGesture -int 2
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadTwoFingerDoubleTapGesture -bool true
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadTwoFingerFromRightEdgeSwipeGesture = 3;
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad USBMouseStopsTrackpad -bool false
 
 
-killall Dock
+# Generic Settings
+## Save screenshots in lossless PNG format
+## rather than defaul JPG (other options: BMP, GIF, JPG, PDF, TIFF)
+defaults write com.apple.screencapture type -string "png"
+## Require password immediately after sleep or screen saver begins
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+## Save screenshots to the desktop
+#defaults write com.apple.screencapture location -string "${HOME}/Desktop"
+## This makes your Mac run quicker (alledgely, not sure if I believe but you can activate if you want):
+## Hide All Desktop Icons from http://osxdaily.com/2009/09/23/hide-all-desktop-icons-in-mac-os-x/
+## The files are still in Desktop directory, but they are now not shown.
+# defaults write com.apple.finder CreateDesktop -bool false;killall Finder
+
+## Fancy screen saver like apple tv. Combines with Aerial from brew install
+## Got settings from: defaults -currentHost write com.apple.screensaver https://apple.stackexchange.com/a/283374
+defaults -currentHost write com.apple.screensaver "{
+    CleanExit = YES;
+    PrefsVersion = 100;
+    idleTime = 600;
+    moduleDict =     {
+        moduleName = Aerial;
+        path = \"${HOME}/Library/Screen Savers/Aerial.saver\";
+        type = 0;
+    };
+    showClock = 0;
+}"
+
+
+
+
+killall Dock; killall Finder
 
 # Set Start Up Apps
 osascript -e 'tell application "iTerm" to open the startup disk'
