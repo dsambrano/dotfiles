53da5b46d4d2410d1ee3280730c63d474e5279be
Deshawn Sambrano <dsambrano@users.noreply.github.com>
2022-10-17 21:14:54 -0700

    Setting Default extensions for apps

--- mac/install.sh
+++ mac/install.sh
@@ -5,8 +5,8 @@ set -e
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 xcode-select --install
 
-brew install bat neofetch fzf htop gh git cmatrix xclip
-brew install --cask r rstudio amethyst mpv visual-studio-code 
+brew install git gh bat neofetch fzf htop cmatrix xclip node ffmpeg duti
+brew install --cask r rstudio amethyst mpv visual-studio-code sublime-text iterm2 firefox brave-browser xquartzdocker zoom slack spotify anaconda
 
 git config --global user.name 'Deshawn Sambrano'
 git config --global user.email ''
@@ -28,6 +28,8 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 # Set up Vim
 echo "syntax on
 set number" >> ~/.vimrc # If I want to move this to ~/.config I can ln -s or follow this guide: https://vi.stackexchange.com/a/11881 
+
+duti -s $(osascript -e 'id of app "Visual Studio Code"') .md all # found from https://superuser.com/a/1092184 in comments
 
 # Anime from Terminal
 curl "https://raw.githubusercontent.com/whoisYoges/anime-terminal/master/anime-terminal" > anime-terminal
