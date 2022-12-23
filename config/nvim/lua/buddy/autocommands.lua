d5e190fbe60bd4cb370fca43ff2bbf6773f732d1
Deshawn Sambrano <dsambrano@users.noreply.github.com>
2022-12-23 10:00:25 -0800

    Setting Markdown and JS to 2 spaces instead of 4

--- config/nvim/lua/buddy/autocommands.lua
+++ config/nvim/lua/buddy/autocommands.lua
@@ -11,3 +11,5 @@ vim.api.nvim_create_autocmd(
 )
 vim.cmd[[autocmd FileType html setlocal shiftwidth=2 tabstop=2 softtabstop=2]]
 vim.cmd[[autocmd FileType css setlocal shiftwidth=2 tabstop=2 softtabstop=2]]
+vim.cmd[[autocmd FileType markdown setlocal shiftwidth=2 tabstop=2 softtabstop=2]]
+vim.cmd[[autocmd FileType javascript setlocal shiftwidth=2 tabstop=2 softtabstop=2]]
