35ee1f06c637d4a7b7a29a00fc0293f5df6f9bfe
Deshawn Sambrano <dsambrano@users.noreply.github.com>
2022-12-23 09:53:43 -0800

    Setting HTML and CSS to 2 spaces instead of 4

--- config/nvim/lua/buddy/autocommands.lua
+++ config/nvim/lua/buddy/autocommands.lua
@@ -9,3 +9,5 @@ vim.api.nvim_create_autocmd(
     { "BufRead", "BufNewFile" },
     { pattern = { "*.txt", "*.md", "*.tex" }, command = "setlocal spell" }
 )
+vim.cmd[[autocmd FileType html setlocal shiftwidth=2 tabstop=2 softtabstop=2]]
+vim.cmd[[autocmd FileType css setlocal shiftwidth=2 tabstop=2 softtabstop=2]]
