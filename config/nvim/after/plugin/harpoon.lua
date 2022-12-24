76325034f43037dae2ea2348ffd92d2edbcb8213
Deshawn Sambrano <dsambrano@users.noreply.github.com>
2022-12-23 16:42:45 -0800

    Adding Harpoon Keymap descriptsion

--- config/nvim/after/plugin/harpoon.lua
+++ config/nvim/after/plugin/harpoon.lua
@@ -7,10 +7,10 @@ if not ok then
 	return
 end
 
-vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "[A]dd Buffer to Harpoon" })
+vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "[E]xplore Harpoon Menu" })
 
-vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-g>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-b>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end, { desc = "Harpoop Navigate to File 1" })
+vim.keymap.set("n", "<C-g>", function() ui.nav_file(2) end, { desc = "Harpoop Navigate to File 2" })
+vim.keymap.set("n", "<C-b>", function() ui.nav_file(3) end, { desc = "Harpoop Navigate to File 3" })
+vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end, { desc = "Harpoop Navigate to File 4" })
