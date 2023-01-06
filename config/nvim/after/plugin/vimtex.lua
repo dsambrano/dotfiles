d306916c25a8cf486e5240bcea3d2d26ee045977
Deshawn Sambrano <dsambrano@users.noreply.github.com>
2023-01-06 09:51:06 -0800

    New Feature: Automatic OS detection for PDF reader on vimtex

--- config/nvim/after/plugin/vimtex.lua
+++ config/nvim/after/plugin/vimtex.lua
@@ -1,1 +1,10 @@
 vim.g.vimtex_view_method = 'zathura' -- zathura for Linux; skim for MacOS
+
+-- Sources:
+--        https://stackoverflow.com/a/2842811
+--        https://www.reddit.com/r/neovim/comments/pc7in0/detect_os_in_lua/
+if vim.fn.has('macunix') then -- gui_win32 for windows and gui_gtk2 || gui_gtk3 for linux
+    vim.g.vimtex_view_method = 'skim' -- zathura for Linux; skim for MacOS
+end
+
+-- print(vim.g.vimtex_view_method)
