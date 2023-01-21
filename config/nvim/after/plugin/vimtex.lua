c3a48820e2ec7f80f1d2a72bfdf0950dd7bac573
Deshawn Sambrano <dsambrano@users.noreply.github.com>
2023-01-20 16:17:30 -0800

    Fix: Switch to Zathura for MacOS on Vimtex

--- config/nvim/after/plugin/vimtex.lua
+++ config/nvim/after/plugin/vimtex.lua
@@ -3,9 +3,9 @@ vim.g.vimtex_view_method = 'zathura' -- zathura for Linux; skim for MacOS
 -- Sources:
 --        https://stackoverflow.com/a/2842811
 --        https://www.reddit.com/r/neovim/comments/pc7in0/detect_os_in_lua/
-if vim.fn.has('macunix') > 0 then -- gui_win32 for windows and gui_gtk2 || gui_gtk3 for linux
-    vim.g.vimtex_view_method = 'skim' -- zathura for Linux; skim for MacOS
-end
+-- if vim.fn.has('macunix') > 0 then -- gui_win32 for windows and gui_gtk2 || gui_gtk3 for linux
+    -- vim.g.vimtex_view_method = 'skim' -- zathura for Linux; skim for MacOS
+-- end
 
 -- see more here: https://github.com/lervag/vimtex/blob/master/doc/vimtex.txt#L979
 -- Probs should make this only the aux files: https://tex.stackexchange.com/a/83290
