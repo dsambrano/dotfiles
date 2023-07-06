3b250c55f6abfe2dc03f5ff6fec577725e1a8478
dsambrano <dsambrano@users.noreply.github.com>
2023-07-06 13:38:14 -0700

    Fix: Switched from vim-plug to packer for installs to make nvim config

--- install.sh
+++ install.sh
@@ -68,9 +68,13 @@ cd -
 # Tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-# Install vim-plug to use plugins for nvim: https://github.com/junegunn/vim-plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# # Install vim-plug to use plugins for nvim: https://github.com/junegunn/vim-plug
+# sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+#        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# Using this to swtich to packer for nvim:
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+	 ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 # Sym links require the clone of the main repo first to work. 
 source ./symlinks.sh $OS_DIR
 
