6fbdcbe9c75553ea9d8ec361d423f585aef635ef
Deshawn Sambrano <dsambrano@users.noreply.github.com>
2023-08-06 12:31:24 -0700

    Fix: Updated output dir location

--- config/nvim/after/plugin/vimtex.lua
+++ config/nvim/after/plugin/vimtex.lua
@@ -10,12 +10,21 @@ vim.g.vimtex_view_method = 'zathura' -- zathura for Linux; skim for MacOS
 -- see more here: https://github.com/lervag/vimtex/blob/master/doc/vimtex.txt#L979
 -- Probs should make this only the aux files: https://tex.stackexchange.com/a/83290
 -- Helps but doesnt move .fls files for some reasons
-vim.g.vimtex_compiler_latexmk = {options = {
-    "-auxdir="..vim.fn.expand("%:t:r").."-build_dir",
-    "-synctex=1",
-    "-interaction=nonstopmode",
-    "-file-line-error"}
+-- vim.g.vimtex_latexmk_build_dir = "build_dir-BUILD"
+vim.g.vimtex_compiler_latexmk = {
+    out_dir = vim.fn.expand("%:t:r").."-build-dir"
 }
+-- vim.g.vimtex_compiler_latexmk = {
+    -- -- build_dir = 'build', vimtex-compiler-latexmk
+    -- -- 'out_dir' = { "-build_dir" },
+    -- -- {'out_dir': {-> vim.fn.expand("%:t:r")}},
+    -- options = {
+    -- -- "-auxdir="..vim.fn.expand("%:t:r").."-build_dir",
+    -- "-outdir="..vim.fn.expand("%:t:r").."-build_dir",
+    -- "-synctex=1",
+    -- "-interaction=nonstopmode",
+    -- "-file-line-error"}
+-- }
 -- Need to switch to this: `expand("%:t:r") .. "-build_dir"` so that you can have custom build dir
 -- vim.g.vimtex_latexmk_build_dir = "build_dir"
 -- vim.g.vimtex_compiler_latexmk = { 
