326aa25dcb05c0487eecf4d7d1f66df49474155f
dsambrano <dsambrano@users.noreply.github.com>
2025-09-20 23:16:16 -0700

    Feat: Installing all tmux packages from command line

--- install.sh
+++ install.sh
@@ -69,6 +69,16 @@ if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
+# Source your tmux config
+tmux source-file ~/.tmux.conf
+
+# Install plugins using TPM (this runs prefix + I command)
+# This is done by calling the install hook directly
+~/.tmux/plugins/tpm/bin/install_plugins
+
+# Optionally, you can also update plugins using:
+# ~/.tmux/plugins/tpm/bin/update_plugins all
+
 # # Install vim-plug to use plugins for nvim: https://github.com/junegunn/vim-plug
 # sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 #        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
