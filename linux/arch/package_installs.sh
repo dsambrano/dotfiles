00f0caec094cb2f07613525e2e52d70001591384
dsambrano <dsambrano@users.noreply.github.com>
2025-10-05 17:58:52 -0700

    Fix: Updating Package Installs

--- linux/arch/package_installs.sh
+++ linux/arch/package_installs.sh
@@ -5,7 +5,7 @@
 ### Dev Only Packages ###
 #########################
 
-sudo pacman -S lsd bat fd fzf jq git-delta zoxide ripgrep cargo neovim git starship fastfetch github-cli stow zsh which alacritty openssh ghostty which man-db tldr npm nodejs wget tmux htop duf
+sudo pacman -S lsd bat fd fzf jq git-delta zoxide ripgrep cargo neovim git starship fastfetch github-cli stow zsh which alacritty openssh ghostty man-db tldr npm nodejs wget tmux # htop duf
 
 # --noconfirm
 
@@ -25,7 +25,7 @@ function full_install() {
 
 # DE
 if full_install; then
-    sudo pacman -S sudo hyprland waybar hypridle hyprpicker hyprlock hyprpolkitagent SwayNotificationCenter xdg-desktop-portal-hyprland xdg-desktop-portal-gtk wofi wireplumber pipewire wl-clipboard flatpak gnome-themes-extra grim networkmanager notofonts noto-fonts-emoji firefox nautilus gdm adw-gtk-theme mpv ocrmypdf fcitx5-anthy wine gamemode mangohud gamescope xdg-utils
+    sudo pacman -S hyprland waybar hypridle hyprpicker hyprlock hyprpolkitagent swaync xdg-desktop-portal-hyprland xdg-desktop-portal-gtk wofi wireplumber pipewire wl-clipboard flatpak gnome-themes-extra grim networkmanager noto-fonts noto-fonts-emoji firefox nautilus gdm adw-gtk-theme mpv fcitx5-anthy wine gamemode mangohud gamescope xdg-utils
 
     # Gnome's new papers Doc viewer (https://archlinux.org/packages/extra/x86_64/papers/):
     # sudo pacman -S papers
@@ -40,7 +40,7 @@ if full_install; then
     sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/paru.git && cd paru && makepkg -si
 
     # Yay installs
-    yay -S brave-bin steam-devices goxlr-utility
+    yay -S brave-bin steam-devices goxlr-utility ocrmypdf 
 
     # Dark Themes https://www.reddit.com/r/hyprland/comments/1h4abmt/comment/lzx4j8g/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
     # gsettings set org.gnome.desktop.interface gtk-theme "adw-gtk3-dark"
